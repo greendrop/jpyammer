695246e48deef925e7d289e54074b0611459864b
greendrop <greendrop@sysgreenplus.net>
2012-03-24 16:03:19 +0900

    rails new jpyammer -T

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jpyammer::Application.config.secret_token = 'd90834b785d149210206ea093ab9deb5b62553f431a005a80e1f351f12ee1e580bcb6cdc971dc89f949f5de8df0132768eb0e10bde28d029432a567d251a96b3'
