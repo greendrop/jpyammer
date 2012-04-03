07bd42e6b403eb01325de734df90b6535bd68007
greendrop <greendrop@sysgreenplus.net>
2012-04-03 23:33:03 +0900

    update view

--- app/controllers/yammer/groups_controller.rb
+++ app/controllers/yammer/groups_controller.rb
@@ -3,7 +3,15 @@ class Yammer::GroupsController < ApplicationController
 
   def groups
     auth = Authentication.find_by_user_id(current_user.id)
-    @groups = Yammer::Groups.get_groups({:access_token => auth.access_token})
+    if auth
+      yammer_params = {
+        :access_token => auth.access_token
+      }
+      @groups = Yammer::Groups.get_groups(yammer_params)
+    else
+      @groups = []
+      flash[:error] = "Don't authenticate Yammer"
+    end
 
     @groups = Kaminari.paginate_array(@groups).page(params[Kaminari.configure { |config| config.param_name }])
 
--- app/controllers/yammer/messages_controller.rb
+++ app/controllers/yammer/messages_controller.rb
@@ -3,15 +3,25 @@ class Yammer::MessagesController < ApplicationController
 
   def my_feed
     auth = Authentication.find_by_user_id(current_user.id)
-    @messages = Yammer::Messages.get_my_feed({:access_token => auth.access_token, :threaded => 'extended'})['messages']
-    @users = {}
-    @messages.each do |message|
-      unless@users.key?(message['sender_id'].to_s)
-        user = Yammer::Users.get_id(message['sender_id'].to_s, {:access_token => auth.access_token})
-        @users[message['sender_id'].to_s] = {}
-        @users[message['sender_id'].to_s]['full_name'] = user['full_name'] 
+    if auth
+      yammer_params = {
+        :access_token => auth.access_token,
+        :threaded => 'extended'
+      }
+      @messages = Yammer::Messages.get_my_feed(yammer_params)['messages']
+      @users = {}
+      @messages.each do |message|
+        unless@users.key?(message['sender_id'].to_s)
+          user = Yammer::Users.get_id(message['sender_id'].to_s, {:access_token => auth.access_token})
+          @users[message['sender_id'].to_s] = {}
+          @users[message['sender_id'].to_s]['full_name'] = user['full_name']
+          @users[message['sender_id'].to_s]['mugshot_url'] = user['mugshot_url']
+        end
+        message['sender_user'] = @users[message['sender_id'].to_s]
       end
-      message['sender_user'] = @users[message['sender_id'].to_s]
+    else
+      @messages = []
+      flash[:error] = "Don't authenticate Yammer"
     end
 
     @messages = Kaminari.paginate_array(@messages).page(params[Kaminari.configure { |config| config.param_name }])
@@ -23,15 +33,25 @@ class Yammer::MessagesController < ApplicationController
 
   def private
     auth = Authentication.find_by_user_id(current_user.id)
-    @messages = Yammer::Messages.get_private({:access_token => auth.access_token, :threaded => 'extended'})['messages']
-    @users = {}
-    @messages.each do |message|
-      unless@users.key?(message['sender_id'].to_s)
-        user = Yammer::Users.get_id(message['sender_id'].to_s, {:access_token => auth.access_token})
-        @users[message['sender_id'].to_s] = {}
-        @users[message['sender_id'].to_s]['full_name'] = user['full_name'] 
+    if auth
+      yammer_params = {
+        :access_token => auth.access_token,
+        :threaded => 'extended'
+      }
+      @messages = Yammer::Messages.get_private(yammer_params)['messages']
+      @users = {}
+      @messages.each do |message|
+        unless@users.key?(message['sender_id'].to_s)
+          user = Yammer::Users.get_id(message['sender_id'].to_s, {:access_token => auth.access_token})
+          @users[message['sender_id'].to_s] = {}
+          @users[message['sender_id'].to_s]['full_name'] = user['full_name'] 
+          @users[message['sender_id'].to_s]['mugshot_url'] = user['mugshot_url']
+        end
+        message['sender_user'] = @users[message['sender_id'].to_s]
       end
-      message['sender_user'] = @users[message['sender_id'].to_s]
+    else
+      @messages = []
+      flash[:error] = "Don't authenticate Yammer"
     end
 
     @messages = Kaminari.paginate_array(@messages).page(params[Kaminari.configure { |config| config.param_name }])
@@ -43,19 +63,25 @@ class Yammer::MessagesController < ApplicationController
 
   def in_group
     auth = Authentication.find_by_user_id(current_user.id)
-    yammer_params = {
+    if auth
+      yammer_params = {
         :access_token => auth.access_token,
         :threaded => 'extended'
       }
-    @messages = Yammer::Messages.get_in_group(params[:id], yammer_params)['messages']
-    @users = {}
-    @messages.each do |message|
-      unless@users.key?(message['sender_id'].to_s)
-        user = Yammer::Users.get_id(message['sender_id'].to_s, {:access_token => auth.access_token})
-        @users[message['sender_id'].to_s] = {}
-        @users[message['sender_id'].to_s]['full_name'] = user['full_name'] 
+      @messages = Yammer::Messages.get_in_group(params[:id], yammer_params)['messages']
+      @users = {}
+      @messages.each do |message|
+        unless@users.key?(message['sender_id'].to_s)
+          user = Yammer::Users.get_id(message['sender_id'].to_s, {:access_token => auth.access_token})
+          @users[message['sender_id'].to_s] = {}
+          @users[message['sender_id'].to_s]['full_name'] = user['full_name'] 
+          @users[message['sender_id'].to_s]['mugshot_url'] = user['mugshot_url']
+        end
+        message['sender_user'] = @users[message['sender_id'].to_s]
       end
-      message['sender_user'] = @users[message['sender_id'].to_s]
+    else
+      @messages = []
+      flash[:error] = "Don't authenticate Yammer"
     end
 
     @messages = Kaminari.paginate_array(@messages).page(params[Kaminari.configure { |config| config.param_name }])
@@ -67,18 +93,24 @@ class Yammer::MessagesController < ApplicationController
 
   def in_thread
     auth = Authentication.find_by_user_id(current_user.id)
-    yammer_params = {
+    if auth
+      yammer_params = {
         :access_token => auth.access_token
       }
-    @messages = Yammer::Messages.get_in_thread(params[:id], yammer_params)['messages']
-    @users = {}
-    @messages.each do |message|
-      unless@users.key?(message['sender_id'].to_s)
-        user = Yammer::Users.get_id(message['sender_id'].to_s, {:access_token => auth.access_token})
-        @users[message['sender_id'].to_s] = {}
-        @users[message['sender_id'].to_s]['full_name'] = user['full_name'] 
+      @messages = Yammer::Messages.get_in_thread(params[:id], yammer_params)['messages'].reverse
+      @users = {}
+      @messages.each do |message|
+        unless@users.key?(message['sender_id'].to_s)
+          user = Yammer::Users.get_id(message['sender_id'].to_s, {:access_token => auth.access_token})
+          @users[message['sender_id'].to_s] = {}
+          @users[message['sender_id'].to_s]['full_name'] = user['full_name'] 
+          @users[message['sender_id'].to_s]['mugshot_url'] = user['mugshot_url']
+        end
+        message['sender_user'] = @users[message['sender_id'].to_s]
       end
-      message['sender_user'] = @users[message['sender_id'].to_s]
+    else
+      @messages = []
+      flash[:error] = "Don't authenticate Yammer"
     end
 
     @messages = Kaminari.paginate_array(@messages).page(params[Kaminari.configure { |config| config.param_name }])
