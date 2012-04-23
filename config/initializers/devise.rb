475de58b2a6e8d710870e4464d0c521e469d120b
greendrop <greendrop@sysgreenplus.net>
2012-04-23 22:34:43 +0900

    update devise

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -211,7 +211,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', :scope => 'user,public_repo'
-  config.omniauth :yammer, ENV['JPYAMMER_YAMMER_KEY'], ENV['JPYAMMER_YAMMER_SECRET']
+  config.omniauth :yammer, ENV['YAMMER_KEY'], ENV['YAMMER_SECRET']
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
